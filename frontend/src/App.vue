e24f9be735a03ee8d3396ad533dd1f2612464d02
milenascc <milenasccarneiro@gmail.com>
2019-12-06 14:59:57 -0300

    Carregando lista de coleções

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,16 +2,37 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <Collection title="Coleções"/>
+      <div v-for="item in collections" v-bind:key="item.id">
+        {{item.name}}
+      </div>
   </div>
 </template>
 
 <script>
 import Collection from './components/Collection.vue'
+import axios from 'axios'
 
 export default {
   name: 'app',
   components: {
     Collection
+  },
+  data: function() {
+    return{
+      collections: []
+    }
+  },
+  mounted(){
+    this.getCollections();
+  },
+  methods:{
+    getCollections: function(){
+      axios.get('http://localhost:3000/api/collection').then((result) =>{
+        this.collections = JSON.parse(JSON.stringify(result.data.results));
+      }).catch((error)=>{
+        console.log(error);
+      });
+    }
   }
 }
 </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,26 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import axios from 'axios'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-  data: {
-    results: []
-  },
-  mounted(){
-    this.getCollections();
-  },
-  methods:{
-    getCollections(){
-      axios.get('http://localhost:3000/api/collection').then((result) =>{
-        //alert(result.data.message);  
-        this.results = result.data.results;
-        console.log(this.results);
-      }).catch((error)=>{
-        console.log(error);
-      });
-    }
-  }
+  render: h => h(App)
 }).$mount('#app')
