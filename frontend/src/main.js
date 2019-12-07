e4ca1703f40a6b73f8cdc13e16d5ee01ce18638e
milenascc <milenasccarneiro@gmail.com>
2019-12-07 07:00:40 -0300

    Configuração inicial para rotas da aplicação

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Disc from './components/Disc'
 
 Vue.config.productionTip = false
 
+//rotas para telas de detalhamento de coleção e de discos, e para a tela inicial
+const routes = [
+  { path: '/disc/:id', component: Disc },
+  { path: '/collection/discs/:id', component: Disc },
+  { path: '/', component: App }
+]
+
+const router = new VueRouter({
+  routes
+});
+
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
